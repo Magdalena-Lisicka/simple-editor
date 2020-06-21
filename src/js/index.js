fc8045e378ba37228527dc24e198c3db9d6b74d6
Magdalena Lisicka <magdalisicka@yahoo.co.uk>
2020-06-21 11:18:27 +0100

    homework done

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,33 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+
+
+
+const textarea = document.querySelector('.textarea--js');
+const save = document.querySelector('.save--js');
+const load = document.querySelector('.load--js');
+
+
+const currentValue = localStorage.getItem('entry');
+
+if (currentValue) {
+    document.querySelector('.info--js').innerHTML = 'i';
+}
+
+
+save.addEventListener('click', (e) => {
+    e.preventDefault();
+    localStorage.setItem('entry',textarea.value);
+    if (textarea.value) {
+        document.querySelector('.info--js').innerHTML = 'i';
+    } else {
+        document.querySelector('.info--js').innerHTML = '';
+    }
+});
+
+load.addEventListener('click', (e) => {
+    e.preventDefault();
+    textarea.value = localStorage.getItem('entry');   
+});
+
